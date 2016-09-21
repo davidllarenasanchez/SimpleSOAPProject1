b6442f0783b2fb962817afea7e222028ecb7704f
DSB <david.llarena.sanchez@gmail.com>
2016-09-21 18:27:12 +0200

    First Commit

---
+++ SimpleSOAPProject1/src/org/project1/webservices/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.project1.webservices;
+
+public class HelloWorld {  
+	  
+    public String sayHelloWorld(String name)  
+    {  
+        return "Hello world from "+ name;  
+    }  
+}  
+
+
